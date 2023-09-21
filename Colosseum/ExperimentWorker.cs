1e99cf34a35f2cfd2fba25a7d175aaec72767fd2
Aleksandr Matsko <a.matsko@g.nsu.ru>
2023-09-21 17:41:43 +0700

    feat: add .NET Host usage bug: calculation completes before starting, stops only bu Ctrl+C

--- Colosseum/ExperimentWorker.cs
+++ Colosseum/ExperimentWorker.cs
@@ -1,25 +1,40 @@
-﻿namespace Colosseum;
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
-public class ExperimentWorker
+namespace Colosseum;
+
+public class ExperimentWorker : BackgroundService
 {
-    public int ExperimentCount { get; }
+    public int ExperimentCount { get; set; } = 1_000_000;
+    private readonly IExperiment _experiment;
+    private readonly ILogger _logger;
 
-    public ExperimentWorker(int experimentCount)
+    public ExperimentWorker(IExperiment experiment, ILogger<ExperimentWorker> logger)
     {
-        ExperimentCount = experimentCount;
+        _experiment = experiment;
+        _logger = logger;
     }
-
-    public int Run(IExperiment experiment)
+    
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
     {
         var success = 0;
-        for (int i = 0; i < ExperimentCount; i++)
+        var experimentsCompleted = 0;
+        for (int i = 0; i < ExperimentCount && !stoppingToken.IsCancellationRequested; i++)
         {
-            if (experiment.Do())
+            if (i % 100000 == 0)
+            {
+                _logger.LogInformation($"Completed {i} iteration");
+            }
+            if (_experiment.Do())
             {
                 success += 1;
             }
+            experimentsCompleted += 1;
         }
         
-        return success;
+        Console.WriteLine($"Experiments completed: {experimentsCompleted}");
+        Console.WriteLine($"Success rate: {(double)success / (experimentsCompleted)}");
+        
+        return Task.CompletedTask;
     }
 }
--- Colosseum/Program.cs
+++ Colosseum/Program.cs
@@ -1,21 +1,27 @@
 ﻿using CardLibrary;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using StrategyLibrary;
 
 namespace Colosseum;
 
 class Program
 {
-    private const int NumExperiments = 1_000_000;
-    
     public static void Main(string[] args)
     {
-        var experiment = new SimpleExperiment(
-            new RandomDeckShuffler(), 
-            new Player("Elon Mask", new PickFirstCardPickStrategy()), 
-            new Player("Mark Zuckerberg", new PickFirstCardPickStrategy())
-            );
-        var worker = new ExperimentWorker(NumExperiments);
-        var success = worker.Run(experiment);
-        Console.WriteLine($"Success rate: {(double)success / NumExperiments}");
+        CreateHostBuilder(args).Build().Run();
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args)
+    {
+        return Host.CreateDefaultBuilder(args)
+            .ConfigureServices((hostContext, services) =>
+            {
+                services.AddHostedService<ExperimentWorker>();
+                services.AddSingleton<IDeckShuffler, RandomDeckShuffler>();
+                services.AddScoped<IExperiment, SimpleExperiment>();
+                services.AddSingleton(provider => new Player("Elon Mask", new PickFirstCardPickStrategy()));
+                services.AddSingleton(provider => new Player("Mark Zuckerberg", new PickFirstCardPickStrategy()));
+            });
     }
 }
