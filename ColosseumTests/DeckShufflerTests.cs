14edfde63b14e5a960f2642707a3f5e8719959f5
Aleksandr Matsko <a.matsko@g.nsu.ru>
2023-09-24 19:22:40 +0700

    test: add Shuffler tests

---
+++ ColosseumTests/DeckShufflerTests.cs
@@ -0,0 +1,31 @@
+﻿using CardLibrary;
+using CardLibrary.Impl;
+using ColosseumTests.DeckTests;
+
+namespace ColosseumTests;
+
+[TestFixture]
+public class DeckShufflerTests
+{
+    [TestCase(10)]
+    [TestCase(36)]
+    public void TestShuffling(int count)
+    {
+        var cards = CardDeckUtils.PrepareCards(count);
+        var deckCards = new Card[cards.Length];
+        cards.CopyTo(deckCards, 0);
+        var deck = new ShuffleableCardDeck(deckCards);
+
+        var shuffler = new RandomDeckShuffler();
+        
+        shuffler.Shuffle(deck);
+
+        for (var i = 0; i < deck.Length; i++)
+        {
+            if (!deck[i].Equals(cards[i]))
+            {
+                Assert.Pass();
+            }
+        }
+    }
+}
