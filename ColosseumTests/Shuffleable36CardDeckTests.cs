29dd5fcc4f317e0478187366c2976c58717c2891
Aleksandr Matsko <a.matsko@g.nsu.ru>
2023-09-24 18:44:03 +0700

    test: add test for Shuffleable36CardDeck

---
+++ ColosseumTests/Shuffleable36CardDeckTests.cs
@@ -0,0 +1,37 @@
+﻿using CardLibrary;
+
+namespace ColosseumTests;
+
+[TestFixture]
+public class Shuffleable36CardDeckTests
+{
+    [Test]
+    public void TestCardsCount()
+    {
+        var deck = Shuffleable36CardDeck.CreateCardDeck();
+        
+        Assert.That(deck.Length, Is.EqualTo(36));
+    }
+
+    [Test]
+    public void TestColorsBalance()
+    {
+        var deck = Shuffleable36CardDeck.CreateCardDeck();
+
+        var blackCount = 0;
+        var redCount = 0;
+        for (var i = 0; i < deck.Length; i++)
+        {
+            if (deck[i].Color == CardColor.Black)
+            {
+                blackCount += 1;
+            }
+            else if (deck[i].Color == CardColor.Red)
+            {
+                redCount += 1;
+            }
+        }
+        
+        Assert.That(blackCount, Is.EqualTo(redCount));
+    }
+}
