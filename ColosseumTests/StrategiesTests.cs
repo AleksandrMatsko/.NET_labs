3b415b6c02fa967dffc4f0bf27b5fb81b1cd0579
Aleksandr Matsko <a.matsko@g.nsu.ru>
2023-09-24 19:02:38 +0700

    test: add strategies tests

---
+++ ColosseumTests/StrategiesTests.cs
@@ -0,0 +1,33 @@
+﻿using CardLibrary;
+using ColosseumTests.DeckTests;
+using StrategyLibrary.Impl;
+
+namespace ColosseumTests;
+
+[TestFixture]
+public class StrategiesTests
+{
+    [TestCase(0)]
+    [TestCase(1)]
+    [TestCase(2)]
+    [TestCase(1_000)]
+    public void TestPickFirstStrategy(int count)
+    {
+        var deck = new CardDeck(CardDeckUtils.PrepareCards(count));
+        var strategy = new PickFirstCardStrategy();
+        
+        Assert.That(strategy.Choose(deck), Is.EqualTo(0));
+    }
+
+    [TestCase(0)]
+    [TestCase(1)]
+    [TestCase(2)]
+    [TestCase(1_000)]
+    public void TestPickLastStrategy(int count)
+    {
+        var deck = new CardDeck(CardDeckUtils.PrepareCards(count));
+        var strategy = new PickLastCardStrategy();
+        
+        Assert.That(strategy.Choose(deck), Is.EqualTo(deck.Length - 1));
+    }
+}
